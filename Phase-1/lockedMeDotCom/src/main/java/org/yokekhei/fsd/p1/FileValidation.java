0431bd7dd0e2083f2dd365771448683cd35cac8b
yokekhei <yokekhei@yahoo.com>
2020-09-28 11:14:34 +0800

    FSD-P1: Refactor FileValidation class.

--- Phase-1/lockedMeDotCom/src/main/java/org/yokekhei/fsd/p1/FileValidation.java
+++ Phase-1/lockedMeDotCom/src/main/java/org/yokekhei/fsd/p1/FileValidation.java
@@ -8,6 +8,41 @@ public class FileValidation {
 	}
 	
 	public boolean isValidAdd(String fileName) throws FileHandlerException {
+		if (isValid(fileName)) {
+			File file = new File(CommonUtils.getFilePath(fileName));
+			
+			// ignore file name case sensitivity check
+			if (file.exists()) {
+				if (file.isDirectory()) {
+					throw new FileHandlerException("There is already a folder with the same "
+							+ "name as the file name you specified. Specify a different name.");
+				} else {
+					throw new FileHandlerException("'" + fileName + "' already exists in the directory.");
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	public boolean isValidDelete(String fileName) throws FileHandlerException {
+		if (isValid(fileName)) {
+			File file = new File(CommonUtils.getFilePath(fileName));
+			
+			if (file.exists() && file.isDirectory()) {
+				throw new FileHandlerException("'" + fileName + "' is a directory. "
+						+ "Delete directory is not supported.");
+			}
+		}
+		
+		return true;
+	}
+	
+	public boolean isValidSearch(String fileName) throws FileHandlerException {
+		return isValid(fileName);
+	}
+	
+	private boolean isValid(String fileName) throws FileHandlerException {
 		if (fileName == null) {
 			throw new FileHandlerException("Invalid null file name.");
 		}
@@ -19,41 +54,6 @@ public class FileValidation {
 		if (fileName.replaceAll("[\\\\/:\\*\\?\\\"<>\\|]", "").length() != fileName.length()) {
 			throw new FileHandlerException("A file name can't contain any of the following characters:"
 					+ System.lineSeparator() + "\\ / : * ? \" < > |");
-		}
-		
-		File file = new File(CommonUtils.getFilePath(fileName));
-		
-		if (file.exists()) {
-			if (file.isDirectory()) {
-				throw new FileHandlerException("There is already a folder with the same "
-						+ "name as the file name you specified. Specify a different name.");
-			} else {
-				throw new FileHandlerException("'" + fileName + "' already exists in the directory.");
-			}
-		}
-		
-		return true;
-	}
-	
-	public boolean isValidDelete(String fileName) throws FileHandlerException {
-		if (fileName == null) {
-			throw new FileHandlerException("Invalid null file name.");
-		}
-		
-		if (fileName.isEmpty()) {
-			throw new FileHandlerException("File name cannot be empty.");
-		}
-		
-		return true;
-	}
-	
-	public boolean isValidSearch(String fileName) throws FileHandlerException {
-		if (fileName == null) {
-			throw new FileHandlerException("Invalid null file name.");
-		}
-		
-		if (fileName.isEmpty()) {
-			throw new FileHandlerException("File name cannot be empty.");
 		}
 		
 		return true;
