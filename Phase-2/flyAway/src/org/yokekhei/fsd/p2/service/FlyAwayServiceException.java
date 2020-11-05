42ad5d0d0b1fab48b8773e62f3a940f650c33fbf
yokekhei <yokekhei@yahoo.com>
2020-11-05 12:08:04 +0800

    FSD-P2: Added exception class for service layer

---
+++ Phase-2/flyAway/src/org/yokekhei/fsd/p2/service/FlyAwayServiceException.java
@@ -0,0 +1,15 @@
+package org.yokekhei.fsd.p2.service;
+
+public class FlyAwayServiceException extends Exception {
+	
+	private static final long serialVersionUID = -7498486058182700625L;
+
+	public FlyAwayServiceException() {
+		super();
+	}
+	
+	public FlyAwayServiceException(final String message) {
+		super(message);
+	}
+	
+}
