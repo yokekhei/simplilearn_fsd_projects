0d8586b10223358c5edf2fcbf5137fe5f2384c34
yokekhei <yokekhei@yahoo.com>
2020-11-05 12:11:39 +0800

    FSD-P2: Added static file path to access JSP/HTML files

---
+++ Phase-2/flyAway/src/org/yokekhei/fsd/p2/ui/servlet/View.java
@@ -0,0 +1,7 @@
+package org.yokekhei.fsd.p2.ui.servlet;
+
+public class View {
+
+	public static final String ADMIN_SIGNIN = "admin/index.jsp";
+	public static final String ADMIN_FLIGHTS_LIST = "admin/flightsList.jsp";
+}
