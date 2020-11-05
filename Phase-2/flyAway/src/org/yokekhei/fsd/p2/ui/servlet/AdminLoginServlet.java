80762431f3181433de57a84f81fb970a58d7e276
yokekhei <yokekhei@yahoo.com>
2020-11-05 12:12:08 +0800

    FSD-P2: Added admin login servlet

---
+++ Phase-2/flyAway/src/org/yokekhei/fsd/p2/ui/servlet/AdminLoginServlet.java
@@ -0,0 +1,51 @@
+package org.yokekhei.fsd.p2.ui.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.yokekhei.fsd.p2.bean.AdminUser;
+import org.yokekhei.fsd.p2.service.AdminService;
+import org.yokekhei.fsd.p2.service.AdminServiceImpl;
+import org.yokekhei.fsd.p2.service.FlyAwayServiceException;
+
+/**
+ * Servlet implementation class AdminLoginServlet
+ */
+@WebServlet("/AdminLoginServlet")
+public class AdminLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminLoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		try {
+			AdminService service = new AdminServiceImpl();
+			AdminUser adminUser = service.login(email, password);
+			HttpSession session = request.getSession();
+			session.setAttribute("adminUser", adminUser);
+			response.sendRedirect("FlightsListServlet");
+		} catch (FlyAwayServiceException e) {
+			e.printStackTrace();
+			response.sendRedirect(View.ADMIN_SIGNIN);
+		}
+	}
+
+}
