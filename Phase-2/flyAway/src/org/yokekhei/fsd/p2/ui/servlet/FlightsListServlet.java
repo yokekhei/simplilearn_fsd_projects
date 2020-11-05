89ebfe83f26c83d479526915f50d42b7b2816a97
yokekhei <yokekhei@yahoo.com>
2020-11-05 12:13:06 +0800

    FSD-P2: Added skeleton flights list servlet

---
+++ Phase-2/flyAway/src/org/yokekhei/fsd/p2/ui/servlet/FlightsListServlet.java
@@ -0,0 +1,37 @@
+package org.yokekhei.fsd.p2.ui.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.yokekhei.fsd.p2.bean.AdminUser;
+
+/**
+ * Servlet implementation class FlightsListServlet
+ */
+@WebServlet("/FlightsListServlet")
+public class FlightsListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FlightsListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		AdminUser adminUser = (AdminUser)session.getAttribute("adminUser");
+		response.sendRedirect(View.ADMIN_FLIGHTS_LIST);
+	}
+
+}
