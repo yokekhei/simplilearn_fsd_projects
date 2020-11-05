c6a3c35f63919325f2464fee7a03a99caedba169
yokekhei <yokekhei@yahoo.com>
2020-11-05 17:05:21 +0800

    FSD-P2: Added servlet to logout

---
+++ Phase-2/flyAway/src/org/yokekhei/fsd/p2/ui/servlet/AdminLogoutServlet.java
@@ -0,0 +1,39 @@
+package org.yokekhei.fsd.p2.ui.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class AdminLogoutServlet
+ */
+@WebServlet("/AdminLogoutServlet")
+public class AdminLogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminLogoutServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			HttpSession session = request.getSession(false);
+			session.invalidate();
+			response.sendRedirect(View.ADMIN_SIGNIN);
+		} catch (Exception e) {
+			Common.viewError(e.getMessage(), request, response);
+		}
+	}
+
+}
