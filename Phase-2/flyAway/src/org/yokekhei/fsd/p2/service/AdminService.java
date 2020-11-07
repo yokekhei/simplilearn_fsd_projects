d4ae05aa2df90656cdfa1ecc5e95ec90040e3ba4
yokekhei <yokekhei@yahoo.com>
2020-11-07 15:24:44 +0800

    FSD-P2: Added service for Place CRUD operations

--- Phase-2/flyAway/src/org/yokekhei/fsd/p2/service/AdminService.java
+++ Phase-2/flyAway/src/org/yokekhei/fsd/p2/service/AdminService.java
@@ -1,9 +1,17 @@
 package org.yokekhei.fsd.p2.service;
 
+import java.util.List;
+
 import org.yokekhei.fsd.p2.bean.AdminUser;
+import org.yokekhei.fsd.p2.bean.Place;
 
 public interface AdminService {
 
 	AdminUser login(String email, String password) throws FlyAwayServiceException;
+	List<Place> getAllPlace() throws FlyAwayServiceException;
+	Place getPlace(String locationCode) throws FlyAwayServiceException;
+	void addPlace(Place data) throws FlyAwayServiceException;
+	void updatePlace(Place data) throws FlyAwayServiceException;
+	void deletePlace(String locationCode) throws FlyAwayServiceException;
 	
 }
--- Phase-2/flyAway/src/org/yokekhei/fsd/p2/service/AdminServiceImpl.java
+++ Phase-2/flyAway/src/org/yokekhei/fsd/p2/service/AdminServiceImpl.java
@@ -1,10 +1,15 @@
 package org.yokekhei.fsd.p2.service;
 
+import java.util.List;
+
 import org.hibernate.SessionFactory;
 import org.yokekhei.fsd.p2.bean.AdminUser;
+import org.yokekhei.fsd.p2.bean.Place;
 import org.yokekhei.fsd.p2.dao.AdminUserDao;
 import org.yokekhei.fsd.p2.dao.AdminUserDaoImpl;
 import org.yokekhei.fsd.p2.dao.FlyAwayDaoException;
+import org.yokekhei.fsd.p2.dao.PlaceDao;
+import org.yokekhei.fsd.p2.dao.PlaceDaoImpl;
 
 public class AdminServiceImpl implements AdminService {
 
@@ -26,6 +31,56 @@ public class AdminServiceImpl implements AdminService {
 		}
 		
 		return adminUser;
+	}
+
+	@Override
+	public List<Place> getAllPlace() throws FlyAwayServiceException {
+		List<Place> places = null;
+		
+		try {
+			PlaceDao dao = new PlaceDaoImpl(sessionFactory);
+			places = dao.getAllPlaces();
+		} catch (FlyAwayDaoException e) {
+			throw new FlyAwayServiceException("Failed to add place - " + e.getMessage());
+		}
+		
+		return places;
+	}
+	
+	@Override
+	public Place getPlace(String locationCode) throws FlyAwayServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void addPlace(Place data) throws FlyAwayServiceException {
+		try {
+			PlaceDao dao = new PlaceDaoImpl(sessionFactory);
+			dao.addPlace(data);
+		} catch (FlyAwayDaoException e) {
+			throw new FlyAwayServiceException("Failed to add place - " + e.getMessage());
+		}
+	}
+	
+	@Override
+	public void updatePlace(Place data) throws FlyAwayServiceException {
+		try {
+			PlaceDao dao = new PlaceDaoImpl(sessionFactory);
+			dao.updatePlace(data);
+		} catch (FlyAwayDaoException e) {
+			throw new FlyAwayServiceException("Failed to update place - " + e.getMessage());
+		}
+	}
+
+	@Override
+	public void deletePlace(String locationCode) throws FlyAwayServiceException {
+		try {
+			PlaceDao dao = new PlaceDaoImpl(sessionFactory);
+			dao.deletePlace(locationCode);
+		} catch (FlyAwayDaoException e) {
+			throw new FlyAwayServiceException("Failed to delete place - " + e.getMessage());
+		}
 	}
 
 }
