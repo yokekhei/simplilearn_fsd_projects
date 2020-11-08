212a7dc01466356df9ecf3f4e53060330c25916f
yokekhei <yokekhei@yahoo.com>
2020-11-09 02:20:34 +0800

    FSD-P2: Added Flight DAO

---
+++ Phase-2/flyAway/src/org/yokekhei/fsd/p2/dao/FlightDao.java
@@ -0,0 +1,15 @@
+package org.yokekhei.fsd.p2.dao;
+
+import java.util.List;
+
+import org.yokekhei.fsd.p2.bean.Flight;
+
+public interface FlightDao {
+
+	void addFlight(Flight data) throws FlyAwayDaoException;
+	List<Flight> getAllFlights() throws FlyAwayDaoException;
+	Flight getFlight(int flightId) throws FlyAwayDaoException;
+	void updateFlight(Flight data) throws FlyAwayDaoException;
+	void deleteFlight(int flightId) throws FlyAwayDaoException;
+	
+}
---
+++ Phase-2/flyAway/src/org/yokekhei/fsd/p2/dao/FlightDaoImpl.java
@@ -0,0 +1,144 @@
+package org.yokekhei.fsd.p2.dao;
+
+import java.util.List;
+import java.util.TimeZone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.yokekhei.fsd.p2.bean.Flight;
+
+public class FlightDaoImpl implements FlightDao {
+
+	private SessionFactory sessionFactory;
+	
+	public FlightDaoImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@Override
+	public void addFlight(Flight data) throws FlyAwayDaoException {
+		Session session = null;
+		Transaction transaction = null;
+		
+		try {
+			session = sessionFactory.withOptions()
+					.jdbcTimeZone(TimeZone.getTimeZone("UTC"))
+					.openSession();
+			transaction = session.beginTransaction();
+			session.save(data);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			
+			throw new FlyAwayDaoException("Failed to add flight - " + e.getMessage());
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}	
+	}
+	
+	@Override
+	public List<Flight> getAllFlights() throws FlyAwayDaoException {
+		Session session = null;
+		List<Flight> flights = null;
+		
+		try {
+			session = sessionFactory.withOptions()
+					.jdbcTimeZone(TimeZone.getTimeZone("UTC"))
+					.openSession();
+			flights = session.createQuery("from Flight").getResultList();
+		} catch (Exception e) {
+			throw new FlyAwayDaoException("Failed to retrieve flights - " + e.getMessage());
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+		
+		return flights;
+	}
+
+	@Override
+	public Flight getFlight(int flightId) throws FlyAwayDaoException {
+		Session session = null;
+		Flight flight = null;
+		
+		try {
+			session = sessionFactory.withOptions()
+						.jdbcTimeZone(TimeZone.getTimeZone("UTC"))
+						.openSession();
+			flight = session.get(Flight.class, flightId);
+			
+			if (flight == null) {
+				throw new FlyAwayDaoException("No flight found for flight id " + flightId);
+			}
+		} catch (Exception e) {
+			throw new FlyAwayDaoException("Failed to query flight details - " + e.getMessage());
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+		
+		return flight;
+	}
+
+	@Override
+	public void updateFlight(Flight data) throws FlyAwayDaoException {
+		Session session = null;
+		Transaction transaction = null;
+		
+		try {
+			session = sessionFactory.withOptions()
+					.jdbcTimeZone(TimeZone.getTimeZone("UTC"))
+					.openSession();
+			transaction = session.beginTransaction();
+			session.update(data);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			
+			throw new FlyAwayDaoException("Failed to update flight - " + e.getMessage());
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+	@Override
+	public void deleteFlight(int flightId) throws FlyAwayDaoException {
+		Session session = null;
+		Transaction transaction = null;
+		
+		try {
+			session = sessionFactory.withOptions()
+					.jdbcTimeZone(TimeZone.getTimeZone("UTC"))
+					.openSession();
+			transaction = session.beginTransaction();
+			Query query = session.createQuery("delete Flight where flight_id=:flightId");
+			query.setParameter("flightId", flightId);
+			query.executeUpdate();
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			
+			throw new FlyAwayDaoException("Failed to delete flight for flight id " +
+					flightId + " - " + e.getMessage());
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+}
