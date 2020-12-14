00d4dc6415eb37934a5beff1474c330155acd85f
yokekhei <yokekhei@yahoo.com>
2020-12-14 13:04:10 +0800

    FSD-P3: Added common and view constants

---
+++ Phase-3/sportyShoes/src/main/java/org/yokekhei/fsd/p3/Common.java
@@ -0,0 +1,10 @@
+package org.yokekhei.fsd.p3;
+
+public class Common {
+
+	public static final String GENDER_MALE = "M";
+	public static final String GENDER_FEMALE = "F";
+	public static final String ROLE_ADMIN = "A";
+	public static final String ROLE_USER = "U";
+	
+}
---
+++ Phase-3/sportyShoes/src/main/java/org/yokekhei/fsd/p3/ui/View.java
@@ -0,0 +1,13 @@
+package org.yokekhei.fsd.p3.ui;
+
+public class View {
+	
+	public static final String V_USER_HOME = "index";
+	
+	public static final String V_ADMIN_HOME = "admin";
+	public static final String V_ADMIN_SIGNIN = "admin/index";
+	public static final String V_ADMIN_CATEGORY_LIST = "admin/category_list";
+	
+	public static final String C_ADMIN_CATEGORY = "admin/category";
+	
+}
