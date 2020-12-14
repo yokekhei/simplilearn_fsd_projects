ef514e9a602f223e5a44701d78dd07c857a4a359
yokekhei <yokekhei@yahoo.com>
2020-12-14 13:03:20 +0800

    FSD-P3: Create SQL Users table and populate initial data

---
+++ Phase-3/sportyShoes/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+USE SportyShoes;
+delete from users;
+insert into users (user_email, user_password, user_role)
+values ('admin@sportyshoes.com', 'Pa$sw0rd', 'A');
--- Phase-3/sportyShoes/src/main/resources/schema.sql
+++ Phase-3/sportyShoes/src/main/resources/schema.sql
@@ -5,3 +5,16 @@ DROP USER IF EXISTS 'sportyshoes_user'@'localhost';
 CREATE USER 'sportyshoes_user'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON * . * TO 'sportyshoes_user'@'localhost';
 FLUSH PRIVILEGES;
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
+    user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    user_fname VARCHAR(255),
+    user_lname VARCHAR(255),
+    user_dob DATE,
+    user_gender CHAR(1),
+    user_role CHAR(1) NOT NULL,
+    user_enabled BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_email)
+) ENGINE=INNODB;
