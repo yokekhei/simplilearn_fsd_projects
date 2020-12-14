9c6c518a2f9c6fe243f914fc3de63b4eae58be18
yokekhei <yokekhei@yahoo.com>
2020-12-14 13:10:12 +0800

    FSD-P3: Added service custom exception

---
+++ Phase-3/sportyShoes/src/main/java/org/yokekhei/fsd/p3/service/SportyShoesServiceException.java
@@ -0,0 +1,15 @@
+package org.yokekhei.fsd.p3.service;
+
+public class SportyShoesServiceException extends Exception {
+	
+	private static final long serialVersionUID = 3044677141790229483L;
+
+	public SportyShoesServiceException() {
+		super();
+	}
+	
+	public SportyShoesServiceException(String message) {
+		super(message);
+	}
+	
+}
