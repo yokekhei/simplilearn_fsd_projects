499b812e30a5d4711836871484c9808c384f6ba0
yokekhei <yokekhei@yahoo.com>
2020-12-16 00:27:30 +0800

    FSD-P3: Added string to local date common method

--- Phase-3/sportyShoes/src/main/java/org/yokekhei/fsd/p3/Common.java
+++ Phase-3/sportyShoes/src/main/java/org/yokekhei/fsd/p3/Common.java
@@ -1,10 +1,24 @@
 package org.yokekhei.fsd.p3;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Common {
 
 	public static final String GENDER_MALE = "M";
 	public static final String GENDER_FEMALE = "F";
 	public static final String ROLE_ADMIN = "A";
 	public static final String ROLE_USER = "U";
+	
+	public static final String DATE_FORMAT = "dd-MM-yyyy";
+	
+	public static LocalDate toLocalDate(String strDate) {
+		return Common.toLocalDate(strDate, DATE_FORMAT);
+	}
+	
+	public static LocalDate toLocalDate(String strDate, String format) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+		return LocalDate.parse(strDate, formatter);
+	}
 	
 }
