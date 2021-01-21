7208eb4e523f1e3b55c88c24299bd499f6e8f22c
yokekhei <yokekhei@yahoo.com>
2021-01-22 01:28:44 +0800

    FSD-P4: [API] Added REST controller for online test categories

---
+++ Phase-4/onlineTest/api/src/main/java/org/yokekhei/fsd/p4/api/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package org.yokekhei.fsd.p4.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yokekhei.fsd.p4.api.entity.Category;
+import org.yokekhei.fsd.p4.api.exception.OnlineTestServiceException;
+import org.yokekhei.fsd.p4.api.service.CommonService;
+
+@RestController
+@RequestMapping(value = "/api",
+				produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+public class CategoryController {
+
+	@Autowired
+	private CommonService commonService;
+	
+	@GetMapping("/category")
+	public List<Category> getCategories() throws OnlineTestServiceException {
+		return commonService.getCategories();
+	}
+	
+}
