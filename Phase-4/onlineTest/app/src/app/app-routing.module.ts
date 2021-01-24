82238abb9394a73c60077055feba47bb1084cb3e
yokekhei <yokekhei@yahoo.com>
2021-01-25 02:03:44 +0800

    FSD-P4: [APP] Added missing tester logout route

--- Phase-4/onlineTest/app/src/app/app-routing.module.ts
+++ Phase-4/onlineTest/app/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { TesteeRegisterComponent } from './components/testee/testee-register/tes
 import { TesterComponent } from './components/tester/tester/tester.component';
 import { TesterHomeComponent } from './components/tester/tester-home/tester-home.component';
 import { TesterLoginComponent } from './components/tester/tester-login/tester-login.component';
+import { TesterLogoutComponent } from './components/tester/tester-logout/tester-logout.component';
 import { TesterRegisterComponent } from './components/tester/tester-register/tester-register.component';
 
 const routes: Routes = [
@@ -32,6 +33,7 @@ const routes: Routes = [
   { path: 'tester', component: TesterComponent,
     children: [
       { path: 'login', component: TesterLoginComponent },
+      { path: 'logout', component: TesterLogoutComponent },
       { path: 'register', component: TesterRegisterComponent },
       { path: 'home', component: TesterHomeComponent, canActivate: [ AuthGuard ] },
       { path: '**', redirectTo: '/tester/login', pathMatch: 'full' }
