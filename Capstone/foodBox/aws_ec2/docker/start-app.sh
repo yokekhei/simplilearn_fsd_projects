4cc0e47436539e0252c06d8d140e355420e78b04
yokekhei <yokekhei@yahoo.com>
2021-03-20 22:04:02 +0800

    FSD-CPS: [DevOps] Added docker related scripts to be run on AWS EC2 instance for deployment

---
+++ Capstone/foodBox/aws_ec2/docker/start-app.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Docker tag name is required"
+    exit 2
+fi
+
+TAG_NAME=$1
+
+docker kill $(docker container ls -a | grep foodbox-app | awk '{print $1}')
+docker rm $(docker container ls -a | grep foodbox-app | awk '{print $1}')
+docker run -it --rm -d --network host -p 4200:4200 --name foodbox-app yokekhei/foodbox-app:${TAG_NAME}
+
+exit 0
---
+++ Capstone/foodBox/aws_ec2/docker/start-db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker kill $(docker container ls -a | grep foodbox-db | awk '{print $1}')
+docker rm $(docker container ls -a | grep foodbox-db | awk '{print $1}')
+
+sudo rm -rf ${PWD}/db_data
+mkdir -p ${PWD}/db_data
+
+docker run -it --rm -p 3306:3306 --name foodbox-db \
+ -v ${PWD}/db_data:/var/lib/mysql \
+ -e MYSQL_ROOT_PASSWORD=password \
+ -e MYSQL_USER=foodbox_user \
+ -e MYSQL_PASSWORD=password \
+ -d mysql:8.0.23
+
+docker exec -i foodbox-db sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD"' < startup.sql
+
+exit 0
