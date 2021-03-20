e088563b40bd150d47571ae930a73a52eee02f03
yokekhei <yokekhei@yahoo.com>
2021-03-20 16:44:06 +0800

    FSD-CPS: [DevOps] Update AWS EC2 scp and setup script

--- Capstone/foodBox/aws_ec2/scp.sh
+++ Capstone/foodBox/aws_ec2/scp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 user='ec2-user'
-instance='ec2-52-3-253-224.compute-1.amazonaws.com'
+instance='ec2-3-84-148-148.compute-1.amazonaws.com'
 homePath=/home/${user}
 destination=${user}@${instance}:${homePath}
 pemFile=ssh-key.pem
