32ee879503a62a1af9d4b1b777a62b813ac5d329
yokekhei <yokekhei@yahoo.com>
2021-04-03 12:49:32 +0000

    FSD-CPS: [DevOps] Rename docker image name for api and app

--- Capstone/foodBox/bin/docker/app/build.sh
+++ Capstone/foodBox/bin/docker/app/build.sh
@@ -4,6 +4,6 @@ base_dir=$(dirname $0)
 
 ${base_dir}/stop.sh
 docker rmi $(docker image ls -a | grep foodbox-app | awk '{print $3}')
-docker build -t foodbox-app ${base_dir}/../../../app
+docker build -t yokekhei/foodbox-app ${base_dir}/../../../app
 
 exit 0
