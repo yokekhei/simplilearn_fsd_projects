ab471028df11ae1e3d94b06f4974a571902b5f26
yokekhei <yokekhei@yahoo.com>
2021-04-04 03:03:43 +0000

    FSD-CPS: [DevOps] Fixed deployment failures at AWS EC2 instance

--- Capstone/foodBox/aws_ec2/docker/start-api.sh
+++ Capstone/foodBox/aws_ec2/docker/start-api.sh
@@ -10,6 +10,6 @@ TAG_NAME=$1
 docker kill $(docker container ls -a | grep foodbox-api | awk '{print $1}')
 docker rm $(docker container ls -a | grep foodbox-api | awk '{print $1}')
 docker rmi $(docker image ls -a | grep foodbox-api | awk '{print $3}')
-docker run -it --rm -d --network host -p 7070:7070 --env-file ${PWD}/.env --name foodbox-api yokekhei/foodbox-api:${TAG_NAME}
+docker run -it --rm -d --network host -p 7070:7070 --env-file ${PWD}/env --name foodbox-api yokekhei/foodbox-api:${TAG_NAME}
 
 exit 0
