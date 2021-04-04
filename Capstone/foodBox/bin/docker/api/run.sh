f9a8e7cc325a304330b0ab864876b7fb06049dc4
yokekhei <yokekhei@yahoo.com>
2021-04-04 03:00:58 +0000

    FSD-CPS: [DevOps] Include env file when starting api docker container

--- Capstone/foodBox/bin/docker/api/run.sh
+++ Capstone/foodBox/bin/docker/api/run.sh
@@ -4,6 +4,6 @@ base_dir=$(dirname $0)
 
 ${base_dir}/stop.sh
 
-docker run -it --rm -d --network host -p 7070:7070 --name foodbox-api yokekhei/foodbox-api
+docker run -it --rm -d --network host -p 7070:7070 --env-file ${PWD}/env --name foodbox-api yokekhei/foodbox-api
 
 exit 0
